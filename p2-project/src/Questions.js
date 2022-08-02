d6f71c905b2b4cf163fbcbc052f37433988f2778
Charles <c_onyemem@live.com>
2022-08-02 13:50:46 -0700

    Created state and used map method show list of questions

--- p2-project/src/Questions.js
+++ p2-project/src/Questions.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
  function Questions() {
+
+  const [questions, setQuestions] = useState([])
+  
+  useEffect(() => {
+    fetch('http://localhost:3000/questions')
+    .then(res => res.json())
+    .then(data => setQuestions(data))
+  }, [])
+
+  const questionsList = questions.map((q)=> < Question key={q.id} />)
+
+
   return (
-    <div>Questions</div>
+    <div className='Questions'>
+      {questionList}
+
+    </div>
   )
 }
 
